e4f6dc0679269c84f7b47fcfdd7a37f83d722d2b
Pedro Callil <pedrocallil@usp.br>
2021-06-03 17:30:17 -0300

    Simplified data initialization for models to be fitted.

--- src/fit_executable/read_file.c
+++ src/fit_executable/read_file.c
@@ -157,9 +157,7 @@ void init_data ( char *model, double *x_init, int p ) {
 
 	if ( strcmp ( model, "uniquac" ) == TRUE ) {
 		n = ( p / 2 );
-		x_init[0] = 1;
-		x_init[n] = 1000;
-		for ( i = 1; i < n; i++ ) {
+		for ( i = 0; i < n; i++ ) {
 			x_init[i] = 10;
 			x_init[n+i] = 1000;
 		}
